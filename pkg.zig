420e38c1c948c78adcb44887ab5689320470c4c2
wozeparrot <wozeparrot@gmail.com>
2020-07-08 19:27:10 -0400

    fix packaging

--- pkg.zig
+++ pkg.zig
@@ -2,11 +2,15 @@ const std = @import("std");
 const LibExeObjStep = std.build.LibExeObjStep;
 const Package = std.build.Pkg;
 
-pub fn Pkg(zva_path: comptime []const u8) type {
+pub fn Pkg(zva_path: comptime []const u8, vk_path: comptime []const u8) type {
     return struct {
         pub const pkg = Package{
             .name = "zva",
             .path = zva_path ++ "/src/lib.zig",
+            .dependencies = &[_]Package{Package{
+                .name = "vk",
+                .path = vk_path,
+            }},
         };
 
         pub fn add(step: *LibExeObjStep) void {
